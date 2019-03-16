85885ca0eb518d449be5e8486444a016b3252af7
goropikari <goropikari56@gmail.com>
2019-03-16 16:31:27 +0900

    Implement read_html like pandas

--- README.md
+++ README.md
@@ -4,3 +4,42 @@
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/goropikari/ReadHTML.jl?svg=true)](https://ci.appveyor.com/project/goropikari/ReadHTML-jl)
 [![Codecov](https://codecov.io/gh/goropikari/ReadHTML.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/goropikari/ReadHTML.jl)
 [![Coveralls](https://coveralls.io/repos/github/goropikari/ReadHTML.jl/badge.svg?branch=master)](https://coveralls.io/github/goropikari/ReadHTML.jl?branch=master)
+
+
+Read HTML table into a `DataFrame`.
+Inspired by [pandas](https://pandas.pydata.org/)
+
+# Installation
+```julia
+import Pkg
+Pkg.pkg"add https://github.com/goropikari/ReadHTML.jl"
+```
+
+# Usage
+```julia
+julia> using ReadHTML
+
+julia> url = "https://gist.githubusercontent.com/goropikari/f02f29e61228a1249626a63187543fbc/raw/04310042022b194a83a2bed7a3c2195ad993f016/ex.html";
+
+julia> read_html(url)
+1-element Array{DataFrames.DataFrame,1}:
+ 3×6 DataFrames.DataFrame. Omitted printing of 4 columns
+│ Row │             No           │             Competition           │
+│     │ Union{Nothing, String}   │ Union{Nothing, String}            │
+├─────┼──────────────────────────┼───────────────────────────────────┤
+│ 1   │             1            │             Swimming              │
+│ 2   │             2            │             Running               │
+│ 3   │             3            │             Shooting              │
+```
+
+This package doesn't support `colspan` and `rowspan`.
+If the headers of tables contain them, you should specify `header=false`.
+```julia
+julia> read_html(url, header=false)
+```
+
+# Other usage
+```julia
+tbs = gettables(url)
+read_table(tbs[1], header=true)
+```
--- src/ReadHTML.jl
+++ src/ReadHTML.jl
@@ -1,5 +1,118 @@
 module ReadHTML
 
-greet() = print("Hello World!")
+using DataFrames, HTTP, Gumbo, AbstractTrees
+export gettables, gettablerow, extractdata, makerowdata, read_html, read_table
+const StrN = Union{String, Nothing}
+
+# url 中の全ての <table>~</table> を抜き出す。
+"""
+    gettables(url::String)
+
+Extract all <table>~</table> contents.
+"""
+function gettables(url::String)
+    data = String(HTTP.get(url).body)
+    data = replace(data, "\n"=>"")
+    table_list = StrN[]
+    offset = 1
+    while true
+        m = match(r"<table.*?>(.*?)</table>"s, data, offset)
+        if isnothing(m)
+            break
+        else
+            push!(table_list, m.match)
+            offset = m.offset + 1
+        end
+    end
+    return table_list
+end
+
+# 表中の全ての行を抜き出す
+"""
+    gettablerow(table)
+
+Extract row data of table.
+"""
+function gettablerow(table, header)
+    l = StrN[]
+    offset = 1
+    while true
+        m = match(r"<tr.*?>(.*?)</tr>"s, table, offset)
+        if isnothing(m)
+            break
+        else
+            offset = m.offset + 1
+            if header
+                push!(l, m.captures[1])
+            elseif !occursin(r"<th.*>", m.captures[1])
+                push!(l, m.captures[1])
+            end
+        end
+    end
+    return l
+end
+
+function extractdata(data)
+    data = replace(data, "\n"=>"")
+    t = parsehtml(data)
+    t = t.root[2]
+
+    str = String[]
+    for elem in PreOrderDFS(t)
+        if isa(elem, HTMLText)
+            push!(str, elem.text)
+        end
+    end
+    return join(str)
+end
+
+function makerowdata(tr)
+    l = StrN[]
+    offset = 1
+    while true
+        m = match(r"<t[dh].*?>(.*?)</t[dh]>"s, tr, offset)
+        if isnothing(m)
+            break
+        else
+            offset = m.offset + 1
+            push!(l, extractdata(m.match))
+        end
+    end
+    return l
+end
+
+function read_table(table; header=true)
+    containheader = occursin("</th>", table)
+    trs = gettablerow(table, header)
+    isempty(trs) && return nothing
+    tr = trs[1]
+    ncol = makerowdata(tr) |> length # Obtain the # of cols
+    nrow = size(trs, 1)
+    arr = Matrix{StrN}(undef, nrow, ncol)
+    for (rowidx, tr) in enumerate(trs)
+        rowdata = makerowdata(tr)
+        for (colidx, colitem) in enumerate(rowdata)
+            arr[rowidx, colidx] = colitem
+        end
+    end
+    try
+        if containheader && header
+            DataFrame(arr[2:end, :], Symbol.(arr[1,:]))
+        else
+            DataFrame(arr)
+        end
+    catch
+    end
+end
+
+function read_html(url; header=true)
+    tables = gettables(url)
+    dfs = DataFrame[]
+    for table in tables
+        tb = read_table(table, header=header)
+        !isnothing(tb) && push!(dfs, tb)
+    end
+    return dfs
+end
 
 end # module
